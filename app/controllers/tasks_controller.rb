2a969d41ea38eaecfc8aa843d4e2347682b9d8e9
Yoshiori SHOJI <yoshiori@gmail.com>
2012-03-13 19:49:54 +0900

    9 章終了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,4 +15,14 @@ class TasksController < ApplicationController
     @task = Task.create(params[:task])
     redirect_to @task
   end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update_attributes(params[:task])
+    redirect_to @task
+  end
 end
