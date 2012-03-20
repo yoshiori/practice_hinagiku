0db71cdfb14776856a083cb995a1c06ed450bd60
Yoshiori SHOJI <yoshiori@gmail.com>
2012-03-20 13:24:45 +0900

    13 章終了

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
+# -*- encoding: utf-8 -*-
+
 module ApplicationHelper
+	def delete_link(object)
+		link_to	"削除", object, :method => :delete,
+		:confirm => "本当に削除しますか？" 
+	end
 end
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,14 @@
+# -*- encoding: utf-8 -*-
 module TasksHelper
+	def links_for(task)
+		links = []
+		links << link_to("修正", [:edit, task])
+		if task.done?
+			links << link_to("戻す", [:restart, task], :method => :put)
+		else
+			links << link_to("完了", [:finish, task], :method => :put)
+		end
+		links << delete_link(task)
+		raw(links.join(" "));
+	end
 end
